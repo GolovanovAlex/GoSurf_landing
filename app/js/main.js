353040f6be872be28a2d09431b12201207137d10
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-10-10 14:35:02 +0300

    css

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,9 @@ $(function () {
   $('.header__slider').slick({
     Infinite: true,
     fade: true,
+    prevArrow:
+      '<img class="slick-arrow slick-arrow__left" src="images/icon/arrow_left.svg" alt="arrow" />',
+    nextArrow:
+      '<img class="slick-arrow slick-arrow__right" src="images/icon/arrow_right.svg" alt="arrow" />',
   });
 });
