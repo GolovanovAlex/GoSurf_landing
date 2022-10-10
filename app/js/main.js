5149c638f272bddb05180eff74c1081e2cd88524
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-10-10 14:51:41 +0300

    slick

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,16 @@ $(function () {
   $('.header__slider').slick({
     Infinite: true,
     fade: true,
+    asNavFor: '.slider-dots',
     prevArrow:
       '<img class="slick-arrow slick-arrow__left" src="images/icon/arrow_left.svg" alt="arrow" />',
     nextArrow:
       '<img class="slick-arrow slick-arrow__right" src="images/icon/arrow_right.svg" alt="arrow" />',
+  });
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    focusOnSelect: true,
   });
 });
