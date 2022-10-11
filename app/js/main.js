8d6b0cb2c5e179aa5c9f17594ad08b914b02062d
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-10-11 08:34:31 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,18 @@
 $(function () {
   $('.header__slider').slick({
-    Infinite: true,
-    fade: true,
     asNavFor: '.slider-dots',
+    infinite: true,
+    fade: true,
     prevArrow:
-      '<img class="slick-arrow slick-arrow__left" src="images/icon/arrow_left.svg" alt="arrow" />',
+      '<img class="slick-arrow slick-arrow__left" src="images/icon/arrow_left.svg" alt="arrow"/img>',
     nextArrow:
-      '<img class="slick-arrow slick-arrow__right" src="images/icon/arrow_right.svg" alt="arrow" />',
+      '<img class="slick-arrow slick-arrow__right" src="images/icon/arrow_right.svg" alt="arrow"/img>',
   });
   $('.slider-dots').slick({
+    asNavFor: '.header__slider',
     slidesToShow: 4,
     slidesToScroll: 4,
-    asNavFor: '.header__slider',
-    focusOnSelect: true,
+    infinite: false,
+    arrows: false,
   });
 });
